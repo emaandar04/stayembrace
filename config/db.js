e54e854aeea06bcac772fadb96a0dc794d25e6dd
Emaan <emaandar04@gmail.com>
2026-08-15 23:14:03 +0500

    Update StayEmbrace project

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
         console.log("✅ MongoDB Connected Successfully");
+        console.log("Host:", conn.connection.host);
+        console.log("Database:", conn.connection.name);
     } catch (error) {
-        console.error("❌ MongoDB Connection Failed:", error.message);
+        console.error(error);
         process.exit(1);
     }
 };
